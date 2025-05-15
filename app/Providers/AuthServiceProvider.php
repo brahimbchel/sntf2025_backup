3c785fd0cef6f4f6730e6dd50cc8a8aab49b2c40
brahimbchel <brahimbchel@gmail.com>
2025-05-15 17:40:44 +0100

    befor switch branch

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,20 +5,38 @@ namespace App\Providers;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
+// use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
+
 class AuthServiceProvider extends ServiceProvider
 {
+    
+
     protected $policies = [
         \App\Models\User::class => \App\Policies\UserPolicy::class,
     ];
 
     public function boot(): void
     {
+        // FilamentShieldPlugin::registerPolicies();
+
         // This lets any logged-in user access Filament
-        Gate::define('access-filament', function ($user) {
-            return true;
-        });
+        // Gate::define('access-filament', function ($user) {
+        //     return true;
+        // });
+
+        // Gate::define('access-filament', function ($user) {
+        //     // Only users with specific roles can access Filament
+        //     return $user->hasRole(['Admin', 'Super Admin']);
+        // });
+
+        Gate::define('access-filament', fn ($user) => $user->hasAnyRole([
+            'Admin',
+            'Super Admin',
+            'Employe', 'employe',
+            'Medecin',
+        ]));
 
         // Optional: restrict to only users with a specific role
-        // Gate::define('access-filament', fn ($user) => $user->hasRole('admin'));
+        // Gate::define('access-filament', fn ($user) => $user->hasRole('Super Admin'));
     }
 }
