3c51846655560f3ebc6130f05676a920d41f649d
brahimbchel <brahimbchel@gmail.com>
2025-05-17 17:43:17 +0100

    add auth api for mobile

--- app/Filament/Resources/AppareilResource.php
+++ app/Filament/Resources/AppareilResource.php
@@ -12,6 +12,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Auth;
 
 class AppareilResource extends Resource
 {
@@ -19,7 +20,11 @@ class AppareilResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-computer-desktop';
 
-    
+        public static function canViewAny(): bool
+    {
+        return Auth::user()?->hasAnyRole(['admin', 'Super Admin', 'admin-agent', 'medecin']) ?? false;
+    }
+
 
     public static function form(Form $form): Form
     {
